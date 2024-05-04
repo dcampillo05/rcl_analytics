c155e824d0c2f69f84a04659be6718dbe7966316
dcampillo05 <dani.campillo05@gmail.com>
2024-05-04 17:57:09 -0300

    dkd

--- src/app/portfolio/(portfolioHome)/page.tsx
+++ src/app/portfolio/(portfolioHome)/page.tsx
@@ -3,14 +3,14 @@
 import Header from "@/app/_Components/header";
 import { motion } from "framer-motion"
 import Image from "next/image";
-import PortItems from "../components/portfolioItems";
+import PortItems from "../portfolioItems";
 
 const Portfolio = () => {
     return (
         <motion.div initial={{ y: 16, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ ease: "easeInOut", duration: 0.75 }}>
             <Header />
             <img src="/portfolioImage.jpg" alt="foto" className="w-full h-[350px]" />
-
+                
             <PortItems />
         </motion.div>
     );
